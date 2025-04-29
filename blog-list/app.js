1f503c2486c322829a6764147869b1ba32f01561
KamiKruse <altsake@protonmail.com>
2025-04-29 12:18:12 +0530

    Completed exercise 4.2 and refactored the bloglist to have the app.js and index.js separated along with the separate controllers and models

---
+++ blog-list/app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('../utils/config')
+const logger = require('../utils/logger')
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+const mongoUrl = config.MONGODB_URI
+const app = express()
+mongoose.set('strictQuery', false)
+
+mongoose
+  .connect(mongoUrl)
+  .then(() => {
+    logger.info('connected to mongoDB')
+  })
+  .catch((error) => {
+    logger.info('error connecting to mongoDB', error.message)
+  })
+
+app.use(express.static('dist'))
+app.use(middleware.requestLogger)
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
